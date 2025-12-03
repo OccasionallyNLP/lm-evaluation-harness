2f0905c115d0007a80d2e2f0c88826dc16100fbf
OccasionallyNLP <tillcry8@gmail.com>
2025-12-04 05:52:35 +0900

    download_datasets.py

---
+++ download_dataset.py
@@ -0,0 +1,14 @@
+allenai/ai2_arc
+winogrande_xl
+EleutherAI/race
+openbookqa
+jet-ai/social_i_qa
+Rowan/hellaswag
+lambada
+baber/piqa
+sciq
+xiaoyuanliu/logiqa
+super_glue, copa
+super_glue, multirc
+nyu-mll/glue, qqp
+cais/mmlu
